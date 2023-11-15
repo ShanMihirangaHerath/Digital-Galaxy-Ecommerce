5b0cbb5f32d2fc6ba05b9aa091a46e38d40b4923
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2023-11-15 18:41:38 +0530

    React Install

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,10 @@
+// This is the recommended router for all React Router web projects.
+import { createBrowserRouter } from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: "Hello",
+    },
+]);
